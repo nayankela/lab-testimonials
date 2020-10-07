e6b4a109d9f8ed0741d7a738904d3d4f2c8ee310
nayankela <nayankela09@gmail.com>
2020-10-07 21:08:57 +0530

    done

---
+++ testimonials/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainComponent,
+    TestimonialComponent,
+    BodyComponent,
+    FooterComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
